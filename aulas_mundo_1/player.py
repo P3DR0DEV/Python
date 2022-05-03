5ca8c53bbc85edaf929cb6ed5b9c5942ae61854c
Pedro <pedrohenriquecamposmendes@hotmail.com>
2022-05-03 20:40:50 -0300

    Update Player

--- aulas_mundo_1/player.py
+++ aulas_mundo_1/player.py
@@ -3,9 +3,13 @@ file_name = 'C:\\Users\\pedro\\Downloads\\msc.mp3'
 pygame.init()
 pygame.mixer.init()
 musica = str(input('Digite música boa: '))
-if musica == 'musica boa': 
-    s = pygame.mixer.Sound(file_name)
-    s.play()
-    s.set_volume(0.1)
-    input('música boa')
-    
+while True:
+    if musica == 'musica boa': 
+        s = pygame.mixer.Sound(file_name)
+        s.play()
+        s.set_volume(0.1)
+        cont = str(input('Tocando...'))
+        if cont == 'Stop':
+            break
+    else:
+        break
