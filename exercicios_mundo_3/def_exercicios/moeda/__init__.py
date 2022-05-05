98c4a65e260b3cacf6c9508d0ccc332203582a60
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-05-05 16:25:00 -0300

    Necessita conserto

--- exercicios_mundo_3/def_exercicios/moeda/__init__.py
+++ exercicios_mundo_3/def_exercicios/moeda/__init__.py
@@ -1,20 +1,23 @@
+def converter(preço=0, moeda = 'R$'):
+    return f'{moeda:.2f}{preço:.2f}'.replace(',', '.')
+
+
 def dobro(n):
     f = n *2
-    return f
+    converter(f)
+
 
 
 def metade(n):
     f = n / 2
-    return f
+    converter(f)
+
 
 
 def aumentar(n, s= 10):
     f =n + (n *s/100)
-    return f
+    converter(f)
 
 def diminuir(n, s= 10):
     f = n - (n * s/100)
-    return f
-
-def moeda(preço=0, moeda = 'R$'):
-    return f'{moeda:.2f}{preço:.2f}' .replace('.', ',')
+    converter(f) 
--- exercicios_mundo_3/ex108.py
+++ exercicios_mundo_3/ex108.py
@@ -1,6 +1,6 @@
 from def_exercicios import moeda
 
 coin = float(input('Digite o preço: R$ '))
-print(f'A metade de {moeda.moeda(coin)} vale {moeda.moeda(moeda.metade(coin))}')
-print(f'O dobro de {moeda.moeda(coin)} vale {moeda.moeda(moeda.dobro(coin))}')
-print(f'Aumentando 10% temos {moeda.moeda(moeda.aumentar(coin))}')
+print(f'A metade de {moeda.converter(coin)} vale {moeda.converter(moeda.metade(coin))}')
+print(f'O dobro de {moeda.converter(coin)} vale {moeda.converter(moeda.dobro(coin))}')
+print(f'Aumentando 10% temos {moeda.converter(moeda.aumentar(coin))}')
