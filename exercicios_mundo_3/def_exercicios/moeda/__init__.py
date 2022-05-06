ec6c6b545e86959e27ef30c08e846f0a1e77ee26
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-05-06 17:28:14 -0300

    Exercício 110

--- exercicios_mundo_3/def_exercicios/moeda/__init__.py
+++ exercicios_mundo_3/def_exercicios/moeda/__init__.py
@@ -24,9 +24,22 @@ def aumentar(n, s= 10, formato = False):
     else:
         return num
 
-def diminuir(n, s= 10, formato = False):
-    num = n - (n * s/100)
+def diminuir(n, sub= 10, formato = False):
+    num = n - (n * sub/100)
     if formato:
         return moeda(num) 
     else:
         return num
+
+
+def resumo(n, s, sub):
+    print('-'*30)
+    print(f'{"RESUMO DO VALOR":^30}')
+    print('-'*30)
+    print(f'Preço Analizado:{moeda(n):>13}')
+    print(f'Dobro do Preço:{dobro(n , True):>14}')
+    print(f'Metade do Preço:{metade(n , True):>13}')
+    print(f'Aumentando {s}%:{aumentar(n , s , True):>14}')
+    print(f'Diminuindo {sub}%:{diminuir(n, sub, True):>14}')
+    print('-'*30)
+
---
+++ exercicios_mundo_3/ex110.py
@@ -0,0 +1,5 @@
+from def_exercicios import moeda
+
+
+num = float(input('Digite um Número para ser analizado: '))
+moeda.resumo(num, 10 , 10)
