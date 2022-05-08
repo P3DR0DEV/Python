6d38c42713820a5d609e577a1471640677413364
Pedro <pedrohenriquecamposmendes@hotmail.com>
2022-05-08 11:18:04 -0300

    ex 113

--- exercicios_mundo_3/def_exercicios/dados.py
+++ exercicios_mundo_3/def_exercicios/dados.py
@@ -1,3 +1,7 @@
+from logging import exception
+from typing import Type, final
+
+
 def leiadinheiro(msg):
     valido = False
     while not valido:
@@ -7,3 +11,30 @@ def leiadinheiro(msg):
         else:
             valido = True
             return float(dado)
+
+
+def leiaint(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('Erro! Digite um número inteiro Válido.')
+            continue
+        except KeyboardInterrupt:
+            print('\nO usuário preferiu não digitar um número')
+            return None
+        else:
+            return n
+ 
+
+def leiafloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except (ValueError, TypeError):
+            print('Erro! O número digitado não é Real.')
+        except KeyboardInterrupt:
+            print('\n O usuário preferiu não informar um dado')
+            return None
+        else:
+            return n 
---
+++ exercicios_mundo_3/ex113..py
@@ -0,0 +1,7 @@
+from def_exercicios.dados import leiaint
+from def_exercicios.dados import leiafloat
+
+
+num = leiaint('Digite um valor Inteiro: ')
+n = leiafloat('Digite um valor Real: ')
+print(f'Os valores digitados foram {num} e {n}!')
