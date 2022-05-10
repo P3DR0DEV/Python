632702806b32115c6df46a8b2b63c4757c2c5ce8
Pedro <pedrohenriquecamposmendes@hotmail.com>
2022-05-09 21:45:10 -0300

    ex115

--- exercicios_mundo_3/ex115.py
+++ exercicios_mundo_3/ex115.py
@@ -1,12 +1,18 @@
-from ex115 import menu
+from ex115 import menu , arquivo
+
+
+if not arquivo.existeArq('Teste.txt'):
+    arquivo.criarArq('Teste.txt')
 
 while True:
     resp = menu.titulo('Ver Usuários Cadastrados', 'Cadastrar um Novo Usuário' , 'Sair do Sistema')
     if resp == 1:
-        print('Opção 1')
+        #lista o conteúdo do arquivo
+        arquivo.lerArq('Teste.txt')
     elif resp == 2:
-        print('Opção 2')
+        arquivo.editArq('Teste.txt')
     elif resp == 3:
+        arquivo.addList('Teste.txt')
         print('\033[31mO usuário escolheu sair.\033[m')
         break
         
