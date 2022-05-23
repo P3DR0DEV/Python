933509cef5770463fcbebc3cc69abe523845cbbe
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-05-23 17:13:29 -0300

    Jogo Update

--- Projeto Jogo/dados/dado.py
+++ Projeto Jogo/dados/dado.py
@@ -8,6 +8,7 @@ def menu(*lista):
         c += 1
     print('--'*20)
     opc = inteiro('Sua Opção: ')
+    print('--'*20)
     return opc
 
 
@@ -18,5 +19,8 @@ def inteiro(msg):
         except (ValueError,TypeError):
             print('Digite um Número inteiro Válido')
             continue
+        except KeyboardInterrupt:
+            print('\033[31mO usuário preferiu sair.\033[m')
+            return None
         else:
             return num
