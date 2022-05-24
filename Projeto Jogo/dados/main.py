9534e0a2c3166d5e0b3e368fb16b4472f9345706
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-05-24 10:36:34 -0300

    Main

--- Projeto Jogo/dados/main.py
+++ Projeto Jogo/dados/main.py
@@ -1,15 +1,21 @@
 from dados import dado, habilidade
 
 
+vida_guerreiro = 100
+vida_mago = vida_arqueiro = 90 
+
 resp = dado.menu('Guerreiro', 'Mago', 'Arqueiro', 'Sair')
 if resp == 1:
     print('Você escolheu a Classe dos Guerreiros.')
-    habilidade.ponto_habilidade(100)
+    habilidade.ponto_habilidade(vida_guerreiro, warrior=0)
+
 elif resp == 2:
     print('Você escolhe a Classe dos Magos.')
-    habilidade.ponto_habilidade(90)
+    habilidade.ponto_habilidade(vida_mago, mago= 1)
+
 elif resp == 3:
     print('Você escolheu a Classe dos Arqueiros.')    
-    habilidade.ponto_habilidade(85)
+    habilidade.ponto_habilidade(vida_arqueiro, arq= 1)
+
 elif resp == 4:
-    print('Você escolheu sair.')
+    print('\033[31mVocê escolheu sair.\033[m')
