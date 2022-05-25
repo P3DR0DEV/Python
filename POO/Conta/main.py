b9e102f94c70b6043c391075a6085b244ed06f19
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-05-25 13:40:57 -0300

    Update

--- POO/Conta/main.py
+++ POO/Conta/main.py
@@ -1,15 +1,13 @@
 from classe import Conta, Cliente
+from funcionarios import Funcionarios,Gerente
 
 cliente1 = Cliente('João', 'Oliveira', '11111111111-11')
 cliente2 = Cliente('José', 'Azevedo', '222222222-22')
 conta1 = Conta('123-4', cliente1.nome, 1000.0)
 conta2 = Conta('123-5', cliente2.nome, 1000.0)
 
-conta1.extrato()
-conta1.sacar(100)
-conta1.extrato()
-conta1.tranfere_para(conta2, 100)
-conta1.extrato()
-conta1.depositar(500)
-conta1.historico.imprime()
-Conta.get_total_contas()
+gerente = Gerente('José','000',5000, '1234s',0)
+print(gerente.get_bonificacao())
+
+funcionario1 = Funcionarios('Pedro', '00002', 2000)
+print(funcionario1.get_bonificacao())
